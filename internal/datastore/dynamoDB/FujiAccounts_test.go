163003903d74505d0e6b34810da8766a80bb78b2
Christopher Sheridan <Christopher.P.Sheridan@aexp.com>
2022-03-04 13:32:07 -0500

    Initial check-in

---
+++ internal/datastore/dynamoDB/FujiAccounts_test.go
@@ -0,0 +1,43 @@
+package dynamoDB
+
+import (
+	"fuji-account/internal/models"
+	"reflect"
+	"testing"
+)
+
+func Test_getItem(t *testing.T) {
+	type args struct {
+		fujiID string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *models.FujiAccount
+		wantErr bool
+	}{
+		{
+			name: "Simple Account ID Test",
+			args: args{
+				fujiID: "1",
+			},
+			want: &models.FujiAccount{
+				FujiID:      "1",
+				AmazonToken: "1",
+				AppleToken:  "1",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getItem(tt.args.fujiID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getItem() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getItem() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
